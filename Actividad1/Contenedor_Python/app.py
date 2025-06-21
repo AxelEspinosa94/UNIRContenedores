d471f703a2b7af5cda9746dde9cb6d02c633daf9
Axel Espinosa <caer23.voltr@outlook.com>
2025-06-20 20:05:06 -0600

    Archivo Python con aplicación Flask que retorna la hora

---
+++ Actividad1/Contenedor_Python/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_time():
+    now = datetime.datetime.now()
+    return f"Current time is: {now}"
+
+if __name__ == "__main__":
+    # Escuchar en todas las interfaces, puerto 5000 (puedes mapearlo a otro en el host)
+    app.run(host="0.0.0.0", port=5000)
